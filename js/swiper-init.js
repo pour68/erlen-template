e57a4ae701b87a8a9bcf096532f9b5dd20bce0a0
pour68 <pour68@outlook.com>
2023-04-26 18:56:40 +0430

    add carousel

--- js/swiper-init.js
+++ js/swiper-init.js
@@ -1,12 +1,26 @@
-// swiper init
-const swiper = new Swiper('.swiper', {
-    // Optional parameters
-    direction: 'vertical',
-    loop: true,
-  
-    // Navigation arrows
-    navigation: {
-      nextEl: '.swiper-button-next',
-      prevEl: '.swiper-button-prev',
-    }
-  });
+let swiperCards = new Swiper('.gallery__cards', {
+  loopedSlide: 5,
+  cssMode: true,
+  effect: "fade",
+});
+
+let swiperThumbnails = new Swiper('.gallery__thumbnails', {
+  loopedSlide: 5,
+  direction: "horizontal",
+  centeredSlides: true,
+  slidesPerView: 3,
+  spaceBetween: 15,
+  paginationClickable: true,
+  slideToClickedSlide: true,
+  watchSlidesProgress: true,
+  pagination: {
+    el: ".swiper-pagination",
+    type: "fraction"
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
+
+swiperThumbnails.controller.control = swiperCards;
